83df63e331f10c145868c21e2e1a861f873c90e2
VuqarAhadli <vuqarahadli17@gmail.com>
2026-07-17 21:14:36 +0400

    1174

---
+++ 1174/1174.sql
@@ -0,0 +1,2 @@
+SELECT  round(100.0 * SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END)  / COUNT(order_date),2) as immediate_percentage  FROM delivery d
+where (customer_id,order_date) in (select customer_id, min(order_date) from delivery group by customer_id)
